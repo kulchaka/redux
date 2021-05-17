fafd9e51fada9deddc46b2b3defe723153482f3e
Andrii <kulchaka@gmail.com>
2021-05-17 11:43:38 +0200

    toggle change Theme

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const count = document.getElementById('counter');
 const addBtn = document.getElementById('add');
 const subBtn = document.getElementById('sub');
 const asyncBtn = document.getElementById('async');
+const themeBtn = document.getElementById('theme');
+const body = document.querySelector('body')
 
 let state = 0
 
@@ -26,6 +28,10 @@ asyncBtn.addEventListener('click', () => {
         state += 100
         render()
     }, 2000)
+})
+
+themeBtn.addEventListener('click', () => {
+    body.classList.toggle('dark')
 })
 
 render()
