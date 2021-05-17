49ffeb92eef1095cddc099be6f9aa499f9cf1705
Andrii <kulchaka@gmail.com>
2021-05-17 11:36:48 +0200

    add business into JS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import './styles.css'
 
 const count = document.getElementById('counter');
+const addBtn = document.getElementById('add');
+const subBtn = document.getElementById('sub');
+const asyncBtn = document.getElementById('async');
 
+let state = 0
 
+function render() {
+    count.textContent = state.toString()
+}
+
+addBtn.addEventListener('click', () => {
+    state++
+    render()
+})
+
+subBtn.addEventListener('click', () => {
+    state--
+    render()
+})
+
+asyncBtn.addEventListener('click', () => {
+    setTimeout(() => {
+        state += 100
+        render()
+    }, 2000)
+})
+
+render()
