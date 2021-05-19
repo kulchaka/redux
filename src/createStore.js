199c5bd695dd396d6357dd7ed8bc40faa6899695
Andrii <kulchaka@gmail.com>
2021-05-19 11:02:44 +0200

    rootReducer

--- src/createStore.js
+++ src/createStore.js
@@ -1,9 +1,11 @@
-export function createStore() {
-    const state = {}
+export function createStore(rootReducer) {
+    let state = {}
     const subscribers = []
     return {
         //action === {type: 'INCREMENT'}
         dispatch(action) {
+            state = rootReducer(state, action)
+            subscribers.forEach(sub => sub())
         },
         subscribe(callback) {
             subscribers.push(callback)
