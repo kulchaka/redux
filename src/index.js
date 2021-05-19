25397e79872d4d7e91ec0903809599bb6f92bc06
Andrii <kulchaka@gmail.com>
2021-05-19 11:16:18 +0200

    Redux done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles.css'
 import {createStore} from "./createStore";
+import {rootReducer} from "./redux/rootReducer";
 
 const counter = document.getElementById('counter');
 const addBtn = document.getElementById('add');
@@ -8,7 +9,9 @@ const asyncBtn = document.getElementById('async');
 const themeBtn = document.getElementById('theme');
 const body = document.querySelector('body')
 
-const store = createStore()
+const store = createStore(rootReducer, 0)
+
+console.log(store.getState())
 
 addBtn.addEventListener('click', () => {
 
